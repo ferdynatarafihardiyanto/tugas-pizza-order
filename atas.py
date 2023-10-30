9cd6e32d0b382edba4b094f81989a4ba8c566cbd
cholifahlabiba <148703340+cholifahlabiba@users.noreply.github.com>
2023-10-30 15:00:30 +0700

    atas.py

---
+++ atas.py
@@ -0,0 +1,23 @@
+#Nama Toko
+print('=' * 30)
+print('          Pizza Hut        ')
+print('=' * 30)
+#Daftar Menu 
+print("Pizza Menu                 Harga")
+print("1. Frankfurter BBQ        :43.000")
+print("2. Meat Monsta            :43.000")
+print("3. Super Supreme          :43.000")
+print("4. Super Supreme Chicken  :43.000")
+#Input 
+pesan_pizza = input("Pilih jenis pizza : ").lower()
+print("Crust Pizza                      Harga")
+print("1. Pan                          : Free")
+print("2. StuffedCrust Cheese          : 12.000")
+print("3. StuffedCrust Sausage         : 9.000")
+print("4. Cheesy Bites                 : 14.000")
+print("5. Crown Crust                  : 12.000")
+crust_pizza = input("Pilih Crust : ").lower()
+print("Ukuran Pizza      Harga")
+print("1. Personal      : Free")
+print("2. Reguler       : 57.000")
+print("3. Large         : 89.000")
